92218ad8d6104d5c66e72c2d5631b0271f728e25
TobiasPressler <pressler.tobias@gmail.com>
2020-07-20 13:47:29 +0200

    initialized workspace

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+define("index", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.sum = void 0;
+    function sum(a, b) {
+        return a + b;
+    }
+    exports.sum = sum;
+});
