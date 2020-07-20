3c25963a416c104a1bf647a163476e0d76429776
TobiasPressler <pressler.tobias@gmail.com>
2020-07-20 16:32:58 +0200

    added an example

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,15 @@
-define("index", ["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.sum = void 0;
-    function sum(a, b) {
-        return a + b;
-    }
-    exports.sum = sum;
-});
+"use strict";
+(function () {
+    L.Stm = L.TileLayer.extend({
+        getTileUrl: function () {
+            var i = Math.ceil(Math.random() * 4);
+            return "https://placekitten.com/256/256?image=" + i;
+        },
+        getAttribution: function () {
+            return "<a href='https://placekitten.com/attribution.html'>PlaceKitten</a>";
+        }
+    });
+    L.stm = function () {
+        return new L.Stm();
+    };
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-export function sum(a: number, b: number) {
-  return a + b;
-}
+//Leaflet
+declare const L: any;
+
+
+//OpenLayers
+
+
+//Leaflet
+(function () {
+  L.Stm = L.TileLayer.extend({
+    getTileUrl: function () {
+      var i = Math.ceil(Math.random() * 4);
+      return "https://placekitten.com/256/256?image=" + i;
+    },
+    getAttribution: function () {
+      return "<a href='https://placekitten.com/attribution.html'>PlaceKitten</a>"
+    }
+  });
+
+  L.stm = function () {
+    return new L.Stm();
+  }
+})();
